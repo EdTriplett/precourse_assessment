1773df1dd06ad1ea672bc24534a55ffd2bb3c9ee
Ed Triplett <EdTriplett@users.noreply.github.com>
2017-07-12 22:26:25 -0400

    removed unnecessary Else block

--- q1_improved.js
+++ q1_improved.js
@@ -4,13 +4,14 @@ var bigPal = function() {
     for (var y = 999; y > 900; y--) {
       if ((x*y).toString() == (x*y).toString().split('').reverse().join('') && (x*y) > max) {
     max=x*y;
-      } else {
-        continue;
-      };
+      } 
     }
   }
   return max;
 }
 
 /* The original version included the extra, unnecessary steps of adding each found palindrome
-to an array and then returning the largest with Math.max */
+to an array and then returning the largest with Math.max 
+-ed.2 - and an unnecessary Else block
+
+*/
