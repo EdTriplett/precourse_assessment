b7f6691d3406fefa7a8fd4d5cff2a665cd7e5916
Ed Triplett <EdTriplett@users.noreply.github.com>
2017-07-12 22:53:29 -0400

    further comments on edits

--- q1_improved.js
+++ q1_improved.js
@@ -11,7 +11,9 @@ var bigPal = function() {
 }
 
 /* The original version included the extra, unnecessary steps of adding each found palindrome
-to an array and then returning the largest with Math.max 
--ed.2 - and an unnecessary Else block
+to an array and then returning the largest with Math.max instead of just storing the highest
+in a variable.  It also included an isPal function to test for palindromes, instead of just 
+including the 'code definition' of a palindrome in the predicate of the if statement.  
 
+-ed.2 - and an unnecessary Else block.  
 */
